dd121e94cd9efc3aeebb8e03ab970d3e13bd133b
onurcankurum <k.onurcan100@gmail.com>
2022-02-12 00:54:23 +0300

    profile page changes

--- lib/view/profile/profile_view_model.dart
+++ lib/view/profile/profile_view_model.dart
@@ -1,7 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:first_three/core/constants/firebase/firebase_constatns.dart';
+import 'package:first_three/core/init/database/firestore_provider.dart';
 import 'package:first_three/model/operations/operation_model.dart';
 import 'package:first_three/model/operations/operation_model_provider.dart';
+import 'package:first_three/model/user/user_model.dart';
+import 'package:first_three/model/user/user_model_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
 part 'profile_view_model.g.dart';
@@ -11,9 +15,38 @@ class ProfileViewModel = _ProfileViewModelBase with _$ProfileViewModel;
 abstract class _ProfileViewModelBase with Store {
   late BuildContext context;
   late String userId;
-
+  UserModel? userModel;
+  UserModelProvider userModelProvider = UserModelProvider();
+  OperationModelProvider operationModelProvider = OperationModelProvider();
+  @observable 
+var operations = ObservableList<OperationModel>();
   void setContext(BuildContext context) async {
     this.context = context;
+    setUserProviderReference();
   }
-  
+  void setUserProviderReference() {
+    CollectionReference<Map<String, dynamic>> collectionReference =
+        FirebaseFirestore.instance.collection(FirebaseConstants.USERS_PATH);
+    userModelProvider.setCollectionReference(collectionReference);
+  }
+    void setOperationProviderReference() {
+    CollectionReference<Map<String, dynamic>> collectionReference =
+        FirebaseFirestore.instance.collection(FirebaseConstants.OPERATIONS_PATH);
+    operationModelProvider.setCollectionReference(collectionReference);
+  }
+  Future<void> getUser()async{
+  userModel= await  userModelProvider.getItem(userId);
+  if(userModel!=null){
+    getUserOperations();
+  }
+  }
+  Future<void>getUserOperations()async{
+    for(String operationId in userModel!.joinedOperations){
+        OperationModel? operationModel = await operationModelProvider.getItem(operationId);
+        if(operationModel!=null){
+          operations.add(operationModel);
+        }
+    }
+  }
+
 }
--- lib/view/profile/profile_view_model.g.dart
+++ lib/view/profile/profile_view_model.g.dart
@@ -9,10 +9,25 @@ part of 'profile_view_model.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$ProfileViewModel on _ProfileViewModelBase, Store {
+  final _$operationsAtom = Atom(name: '_ProfileViewModelBase.operations');
+
+  @override
+  ObservableList<OperationModel> get operations {
+    _$operationsAtom.reportRead();
+    return super.operations;
+  }
+
+  @override
+  set operations(ObservableList<OperationModel> value) {
+    _$operationsAtom.reportWrite(value, super.operations, () {
+      super.operations = value;
+    });
+  }
+
   @override
   String toString() {
     return '''
-
+operations: ${operations}
     ''';
   }
 }
