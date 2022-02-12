7b77af9f55f43fa985f9a1a550190fb748eaeb55
beytullahTopuz <beytullah.topuz37@gmail.com>
2022-02-12 18:07:56 +0300

    firastore

---
+++ lib/core/services/storage/storage_service.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+import 'dart:math';
+
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+
+class StrageService{
+
+final firebase_storage.Reference _storage =
+  firebase_storage.FirebaseStorage.instance.ref();
+  
+
+  final DateTime time = DateTime.now();
+ // String? resimId;
+
+    Future<String> userimgUpload(File imageFile, String id) async {
+      var randomName = getRandomString(10);
+    
+      print("randomname : ");
+      print(randomName);
+
+    firebase_storage.UploadTask uploadTask =
+        _storage.child("images/userpictures/$id.jpg").putFile(imageFile);
+
+    firebase_storage.TaskSnapshot snapshot = await uploadTask;
+
+    String imageURL = await snapshot.ref.getDownloadURL();
+    return imageURL;
+  }
+
+  final _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  final Random _rnd = Random();
+  String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
+}
+
+mixin FirebaseStorage {
+}
--- lib/view/operation_detail/operation_detail_view.dart
+++ lib/view/operation_detail/operation_detail_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:first_three/core/base/state/base_state.dart';
 import 'package:first_three/core/base/view/base_view.dart';
@@ -8,6 +10,7 @@ import 'package:first_three/model/user_officer/user_officer.dart';
 import 'package:first_three/view/operation_detail/operation_detail_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:image_picker/image_picker.dart';
 
 class OperationDetailView extends StatefulWidget {
   const OperationDetailView({Key? key}) : super(key: key);
@@ -20,6 +23,61 @@ class _OperationDetailViewState extends BaseState<OperationDetailView> {
   late OperationDetailViewModel viewModel;
   final f = DateFormat('yyyy-MM-dd hh:mm');
   int rank = 0;
+
+  File? imgFile;
+  String? tempFotoUrl;
+
+
+  void _resimSec(
+      ImageSource source, BuildContext context) async {
+    final picker = ImagePicker();
+    var secilen = await picker.getImage(source: source);
+
+
+      if(secilen != null){
+         imgFile = File(secilen.path);
+         print(secilen.path);
+          Navigator.pop(context);
+
+      //upload firabase
+     //     var url = await StrageService().userimgUpload(imgFile!, userID);
+   
+        // setState(() {});
+      }
+  }
+
+    void _showImageDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              title: const Text("Galeriden Fotoğraf Seç"),
+              onTap: () {
+                _resimSec(ImageSource.gallery, context);
+              },
+            ),
+            ListTile(
+              title: const Text("Kameradan Fotoğraf Çek"),
+              onTap: () {
+                _resimSec(ImageSource.camera, context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return BaseView(
@@ -34,6 +92,8 @@ class _OperationDetailViewState extends BaseState<OperationDetailView> {
         },
         onPageBuilder: (context, value) => scaffold);
   }
+
+
 
   Widget get scaffold => Scaffold(
           body: Stack(
@@ -133,18 +193,24 @@ class _OperationDetailViewState extends BaseState<OperationDetailView> {
             padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Icon(
+              children:  [
+                const Icon(
                   Icons.add,
                   color: Colors.white,
                 ),
-                Text(
+                const Text(
                   "öncesi",
                   style: TextStyle(fontSize: 25),
                 ),
-                Icon(
-                  Icons.add,
-                  size: 27,
+                 GestureDetector(
+                  onTap: () {
+                   _showImageDialog(context);
+                  },
+                  behavior: HitTestBehavior.opaque,
+                  child: const Icon(
+                    Icons.add,
+                    size: 27,
+                  ),
                 )
               ],
             ),
@@ -173,18 +239,22 @@ class _OperationDetailViewState extends BaseState<OperationDetailView> {
             padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Icon(
+              children:  [
+                const Icon(
                   Icons.add,
                   color: Colors.white,
                 ),
-                Text(
+                const Text(
                   "sonrası",
                   style: TextStyle(fontSize: 25),
                 ),
-                Icon(
-                  Icons.add,
-                  size: 27,
+                GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: (){},
+                  child: const Icon(
+                    Icons.add,
+                    size: 27,
+                  ),
                 )
               ],
             ),
